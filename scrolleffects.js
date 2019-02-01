17c2de4eb7a9712a274faf7fca57522843eca791
jrathod9 <lewisburnard@gmail.com>
2019-02-01 13:46:29 +0530

    minor scrolleffect fixes

--- scrolleffects.js
+++ scrolleffects.js
@@ -11,13 +11,13 @@ $(window).scroll(function () {
   // }
   if(parseInt(num)<50){
   	$('#transitimg').css("background-image","url('transit/t3-ConvertImage.jpg')");
-  	$('#transitimg').css("box-shadow","0px 3px 30px #123456");	
-  	$('#transitimg').css("border","0.3px solid white");	
+  	$('#transitimg').css("box-shadow","0px 3px 10px aquamarine");	
+  	// $('#transitimg').css("border","0.3px solid white");	
   }
   else{
   	$('#transitimg').css("background-image","url('transit/t2-ConvertImage.jpg')");
-  	$('#transitimg').css("box-shadow","0px 3px 30px aquamarine");
-  	$('#transitimg').css("border","0.3px solid white");			
+  	$('#transitimg').css("box-shadow","0px 3px 50px aquamarine");
+  	// $('#transitimg').css("border","0.3px solid white");			
   }
     
 });
